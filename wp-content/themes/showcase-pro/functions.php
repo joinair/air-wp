d7ccc6322b507ecdd3ba51c1f6b43d7a5cce5254
raj <rajkumar@sumedhasoftech.com>
2017-05-15 18:41:38 +0530

    1. updated blog post listing sections design. 2. updated Resource posts: Job descriptions, Policy templates, Interview questions design

--- wp-content/themes/showcase-pro/functions.php
+++ wp-content/themes/showcase-pro/functions.php
@@ -374,3 +374,123 @@ foreach($scripts_to_async as $async_script){
 return $tag;
 }
 add_filter( 'script_loader_tag', 'defer_js_async', 10 );
+
+/* ==========================================================================
+ * App landing page related pages
+ * ========================================================================== */
+
+function get_feature_children() {
+  
+	// Set up the objects needed
+	$feature_page_id=7388;
+	$other_air_features='';
+	$current_page_id=get_the_ID();
+	$my_wp_query = new WP_Query();
+	$all_wp_pages = $my_wp_query->query(array('post_type' => 'page','posts_per_page'=>'-1'));
+
+	$childs=get_page_children($feature_page_id,$all_wp_pages);
+	$other_air_features.='<div class="vc_row wpb_row vc_row-fluid">';
+	foreach($childs as $child){
+		
+		 $child_id=$child->ID;
+		$child_image= get_post_meta($child_id,'sub_feature_image_url',true);
+		$child_text= get_post_meta($child_id,'sub_feature_text',true);
+		$child_title= get_post_meta($child_id,'sub_feature_title',true);
+		$child_link=get_permalink( $child_id );
+		if($current_page_id!=$child_id && !empty($child_image) && !empty($child_text) && !empty($child_title) && !empty($child_link)){
+			
+		$other_air_features.='<div class="wpb_column vc_column_container vc_col-sm-4" style="padding: 15px; min-height: 260px;"><p class="p1" style="text-align: center; margin-bottom: 5px;"><a href="'.$child_link.'"><b><img class="aligncenter" src="'.$child_image.'" width="60" height="60" /></b></a></p>
+
+<h4 class="p1" style="text-align: center; margin-bottom: 10px;">'.$child_title.'</h4>
+<p class="p1" style="text-align: center; margin-bottom: 10px;">'.$child_text.'</p>
+<p class="p1 red" style="text-align: center; margin-bottom: 10px;"><a href="'.$child_link.'">Learn more →</a></p></div>';	
+						
+		}
+	}
+	$other_air_features.='</div>';
+	return	$other_air_features;
+  
+  
+   }
+add_shortcode('get_feature_child', 'get_feature_children');
+
+add_action( 'pre_get_posts', 'mjg_show_titles_only_category_pages' );
+function mjg_show_titles_only_category_pages( $query ) {
+ 
+	if( $query->is_main_query() && $query->is_category(37) ) {
+				
+		remove_action( 'genesis_loop', 'genesis_do_loop' );
+		add_action( 'genesis_loop', 'surefire_loop_helper_blog' );
+                
+        }else if($query->is_main_query() && $query->is_category(array(35,33,36)) ){
+            
+                remove_action( 'genesis_loop', 'genesis_do_loop' );
+		add_action( 'genesis_loop', 'surefire_loop_helper_other_cat' );
+        }
+}
+
+
+function surefire_loop_helper_blog() {
+//Create a standard wordpress loop
+    $each_post_data='<div class="vc_row wpb_row column_row-fluid clearfix">';
+while(have_posts()) : the_post();
+
+$each_post_data.=''
+        . '<div class="fourth_column">'
+        . '<div class="blog_box"><a href="'.get_permalink().'" class="text-center read_more">'
+        . '<div class="blog_thumnail">'
+        .'<img class="aligncenter" src="'.get_the_post_thumbnail_url().'" />
+            </div>
+            <div class="blog_details">
+<span class="time">'.get_the_date("d M Y").'</span>
+<h5 class="p1" style="margin-bottom: 10px;">
+'.wp_trim_words( get_the_title(), 9, '...').'</h5>
+<p class="p1" style="margin-bottom: 10px;">'.wp_trim_words( get_the_content(), 15, '...').'</p>
+<span class="read_text_link">Read post →</span>'
+        . '</div>'
+        . '</a></div>'
+        . '</div>';			
+
+
+endwhile;
+echo $each_post_data.'</div>';
+echo the_posts_pagination( array(
+	'mid_size'  => 2,
+	'prev_text' => __( '&#x2190;', 'back' ),
+	'next_text' => __( '&#x2192;', 'onward' ),
+) );
+}
+
+
+function surefire_loop_helper_other_cat() {
+//Create a standard wordpress loop
+    $each_post_data='<div class="vc_row wpb_row column_row-fluid clearfix">';
+while(have_posts()) : the_post();
+
+$each_post_data.=''
+        . '<div class="fourth_column">'
+        . '<div class="blog_box resources_archive_block"><a href="'.get_permalink().'">'
+        . '<div class="blog_thumnail">'
+        . '<img class="aligncenter" height="65px" src="'.get_site_url().'/wp-content/uploads/2017/05/file_icon.png" />'
+        .'<i class="fa fa-file-text-o" aria-hidden="true"></i>
+       
+            
+            </div>
+            <div class="blog_details">
+<span class="time"></span>
+<h5 class="p1" style="margin-bottom: 10px;">
+'.wp_trim_words( get_the_title(), 9, '').'</h5>
+<p class="p1" style="margin-bottom: 10px;">'.wp_trim_words( get_the_content(), 15, '').'</p>'
+        . '</div>'
+        . '</a></div>'
+        . '</div>';			
+
+
+endwhile;
+echo $each_post_data.'</div>';
+echo the_posts_pagination( array(
+	'mid_size'  => 2,
+	'prev_text' => __( '&#x2190;', 'back' ),
+	'next_text' => __( '&#x2192;', 'onward' ),
+) );
+}
